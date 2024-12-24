c07d6628133353ed38a5d915f0c760aedb795285
tiechui1994 <2904951429@qq.com>
2024-12-24 17:17:29 +0800

    add new tool

--- docker/init.sh
+++ docker/init.sh
@@ -6,10 +6,13 @@ export TZ=Asia/Shanghai && \
 apt-get --quiet install --yes openssh-server net-tools iputils-ping iproute2 iptables openssl vim curl
 
 wget https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz -O /tmp/ookla.tgz && \
-tar xf /tmp/ookla.tgz -C /tmp && mv /tmp/speedtest /root
+tar xf /tmp/ookla.tgz -C /tmp && mv /tmp/speedtest /root && rm -rf /tmp/ookla.tgz && rm -rf /tmp/speedtest*
 
 wget https://github.com/yt-dlp/yt-dlp/releases/download/2024.12.13/yt-dlp -O /tmp/yt-dlp && \
 chmod a+x /tmp/yt-dlp && mv /tmp/yt-dlp /root
+
+wget https://go.dev/dl/go1.23.4.linux-amd64.tar.gz -O /tmp/go.tgz && \
+tar xf /tmp/go.tgz -C /tmp && mv /tmp/go /opt && ln -sf /opt/go/bin/go /usr/local/bin && rm -rf /tmp/go.tgz
 
 passwd=$(echo abc123_ |openssl passwd -6 -stdin)
 sed -i -E "/^root/ s|root:([^:]+?):(.*)|root:$passwd:\2|" /etc/shadow && cat > /etc/ssh/sshd_config <<-'EOF'
--- docker/ubuntu.Dockerfile
+++ docker/ubuntu.Dockerfile
@@ -1,12 +1,14 @@
-FROM golang:1.17 AS builder
+FROM golang:1.23 AS builder
 WORKDIR /app
 COPY . .
 RUN go mod tidy && go build -o ./tcpover ./cmd/tcpover/main.go
+RUN go install github.com/anacrolix/torrent/cmd/torrent@latest && mv /go/bin/torrent /app
 
 
 FROM ubuntu:latest
 WORKDIR /app
 COPY --from=builder /app/tcpover .
+COPY --from=builder /app/torrent /root
 COPY --from=builder /app/docker/init.sh .
 RUN bash init.sh
 ENTRYPOINT mkdir /run/sshd && \
