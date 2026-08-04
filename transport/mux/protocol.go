118421ff03e70ce78276d90365bf021d35e4ad19
quinn <2904951429@qq.com>
2026-08-04 20:53:55 +0800

    Update KeepAliveTimeout in DefaultMuxConfig
    Increase KeepAliveTimeout from 20 seconds to 60 seconds.

--- transport/mux/protocol.go
+++ transport/mux/protocol.go
@@ -16,7 +16,7 @@ var DefaultMuxConfig = smux.Config{
 	Version:           1,
 	KeepAliveDisabled: false,            // 这个是 Mihomo 必须设置的
 	KeepAliveInterval: 30 * time.Second, // 无用
-	KeepAliveTimeout:  20 * time.Second, // 无用
+	KeepAliveTimeout:  60 * time.Second, // 无用
 	MaxFrameSize:      8192,             // 8K
 	MaxReceiveBuffer:  4194304,
 	MaxStreamBuffer:   65536, // 用不到
